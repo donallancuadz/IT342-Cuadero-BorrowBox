987d92e3a81d61e7d31a73ea1f8bb9b53bc45515
Don Allan Suarez Cuadero <donallancuadz11@gmail.com>
2026-05-07 22:45:09 +0800

    Updated response DTO and application configuration

--- backend/demo/src/main/java/edu/cit/cuadero/borrowbox/dto/MeResponse.java
+++ backend/demo/src/main/java/edu/cit/cuadero/borrowbox/dto/MeResponse.java
@@ -4,14 +4,17 @@ public class MeResponse {
     private Long id;
     private String fullName;
     private String email;
+    private String role;
 
-    public MeResponse(Long id, String fullName, String email) {
+    public MeResponse(Long id, String fullName, String email, String role) {
         this.id = id;
         this.fullName = fullName;
         this.email = email;
+        this.role = role;
     }
 
     public Long getId() { return id; }
     public String getFullName() { return fullName; }
     public String getEmail() { return email; }
+    public String getRole() { return role; }
 }
